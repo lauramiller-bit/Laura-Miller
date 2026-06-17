0f240e487c06ac74d7c9e45aae74f372ca189614
LAURA MILLER <laura_miller@estudante.sesisenai.org.br>
2026-06-17 15:19:29 -0300

    Atividade 3

--- exercício 1.js
+++ exercício 1.js
@@ -11,3 +11,14 @@ const experiencia = "5 meses";
 console.log("Nome:", nome);
 console.log("Cargo:", cargo);
 console.log("Experiência:", experiencia + " anos");
+
+
+// exercício 3:
+const vendas = 5000;
+const bonificacao = vendas * 0.10; // 10% das vendas
+const total = vendas + bonificacao;
+
+// Exibindo os resultados
+console.log("Vendas: R$ " + vendas);
+console.log("Bonificação: R$ " + bonificacao);
+console.log("Total: R$ " + total);
