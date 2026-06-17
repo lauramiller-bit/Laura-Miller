b9aa3d67706d38ba704f1f43f1ed7a73c7b87bd9
LAURA MILLER <laura_miller@estudante.sesisenai.org.br>
2026-06-17 15:07:54 -0300

    atividade 2

--- exercício 1.js
+++ exercício 1.js
@@ -1,2 +1,13 @@
 // exercício 1:
 console.log("Bem-vindo ao ambiente de desenvolvimento JavaScript!")
+
+
+// exercício 2:
+const nome = "Laura Miller";
+const cargo = "programador";
+const experiencia = "5 meses";
+
+// Exibindo informações
+console.log("Nome:", nome);
+console.log("Cargo:", cargo);
+console.log("Experiência:", experiencia + " anos");
