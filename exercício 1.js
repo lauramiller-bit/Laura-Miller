153f96b5bf78eb181daa976ff9b9f55e356e2875
LAURA MILLER <laura_miller@estudante.sesisenai.org.br>
2026-06-17 16:14:34 -0300

    Atividade 6:

--- exercício 1.js
+++ exercício 1.js
@@ -22,3 +22,53 @@ const total = vendas + bonificacao;
 console.log("Vendas: R$ " + vendas);
 console.log("Bonificação: R$ " + bonificacao);
 console.log("Total: R$ " + total);
+
+
+// exercício 4:
+let idade = "15"
+
+if (idade >= 18) {
+    console.log("Acesso autorizado.");
+} else {
+    console.log("Acesso negado.");
+}
+
+
+//exercício 5:
+let prioridade = 3;
+
+if (prioridade === 1) {
+    console.log("Prioridade: Baixa");
+} else if (prioridade === 2) {
+    console.log("Prioridade: Média");
+} else if (prioridade === 3) {
+    console.log("Prioridade: Alta");
+} else {
+    console.log("Código de prioridade inválido.");
+}
+//
+//
+""
+//exercício 6:
+let opcao = 4;
+
+switch (opcao) {
+    case 1:
+        console.log("Setor: Financeiro");
+        break;
+
+    case 2:
+        console.log("Setor: Comercial");
+        break;
+
+    case 3:
+        console.log("Setor: Suporte Técnico");
+        break;
+
+    case 4:
+        console.log("Setor: Recursos Humanos");
+        break;
+
+    default:
+        console.log("Opção inválida.");
+}
